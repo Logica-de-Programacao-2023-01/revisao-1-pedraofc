bae2a5afa20a33d5447bd05c1c8ae14c5b74245c
KaikeVitorino <kaike.vitorino@sempreceub;com>
2023-04-24 08:01:23 -0300

    casa

--- q1/q1.go
+++ q1/q1.go
@@ -1,6 +1,60 @@
-package q1
+package main
 
-func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+import (
+	"errors"
+	"fmt"
+)
+
+type compra struct {
+	valor float64
+}
+
+type historicoCompras []compra
+
+func (h historicoCompras) valorTotal() float64 {
+	total := 0.0
+	for _, c := range h {
+		total += c.valor
+	}
+	return total
+}
+
+func calcularDesconto(valorCompra float64, historico historicoCompras) (float64, error) {
+	if valorCompra <= 0 {
+		return 0, errors.New("valor da compra inválido")
+	}
+
+	var desconto float64
+
+	if len(historico) == 0 {
+		// primeira compra do cliente
+		desconto = valorCompra * 0.1
+	} else {
+		totalCompras := historico.valorTotal()
+		mediaCompras := totalCompras / float64(len(historico))
+
+		if totalCompras > 1000 {
+			desconto = valorCompra * 0.1
+		} else if totalCompras <= 500 {
+			desconto = valorCompra * 0.02
+		} else {
+			desconto = valorCompra * 0.05
+		}
+
+		if mediaCompras > 1000 && desconto < valorCompra*0.2 {
+			desconto = valorCompra * 0.2
+		}
+	}
+
+	return desconto, nil
+}
+
+func main() {
+	historico := historicoCompras{{valor: 500}, {valor: 750}}
+	desconto, err := calcularDesconto(1000, historico)
+	if err != nil {
+		fmt.Println("Erro:", err)
+	} else {
+		fmt.Println("Desconto:", desconto)
+	}
 }
--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,29 @@
-package q2
+package main
 
-func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+import (
+	"fmt"
+	"strings"
+)
+
+func countAverageWordLength(text string) (float64, error) {
+	words := strings.Fields(text)
+	if len(words) == 0 {
+		return 0, fmt.Errorf("texto vazio")
+	}
+
+	var sum float64
+	for _, word := range words {
+		sum += float64(len(word))
+	}
+	return sum / float64(len(words)), nil
+}
+
+func main() {
+	text := "O rato roeu a roupa do rei de Roma"
+	avgLen, err := countAverageWordLength(text)
+	if err != nil {
+		fmt.Println("Erro:", err)
+	} else {
+		fmt.Printf("Média de letras por palavra: %.1f\n", avgLen)
+	}
 }
